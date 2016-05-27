e3c42bb7b092634b27748b51fba76b160ab77890
天 谢 <jsjtxietian@outlook.com>
2016-05-27 14:22:05 +0800

    particleManger over

--- Classes/Manager.h
+++ Classes/Manager.h
@@ -1,13 +1,14 @@
 ﻿#pragma once
 #include "Helper.h"
 
-extern std::list<std::string> HeroPool;
-extern std::list<std::string> DragonPool;
-extern std::list<std::string> SlimePool;
-extern std::list<std::string> RatPool;
-extern std::list<std::string> BossPool;
-extern std::list<std::string> HeroManager;
-extern std::list<std::string> MonsterManager;
+//extern std::list<std::string> HeroPool;
+//extern std::list<std::string> DragonPool;
+//extern std::list<std::string> SlimePool;
+//extern std::list<std::string> RatPool;
+//extern std::list<std::string> BossPool;
+//extern std::list<std::string> HeroManager;
+//extern std::list<std::string> MonsterManager;
+//extern std::list<std::string> PigletPool;
 
 extern Size size ;
 extern Scheduler* scheduler;
@@ -17,3 +18,4 @@ void collision(/*object*/);
 void isOutOfBound(/*object*/);
 void collisionDetect(float dt);
 Vec2 getFocusPointOfHeros();
+//std::list<std::string> getPoolByName(std::string name);
--- Classes/ParticleManager.cpp
+++ Classes/ParticleManager.cpp
@@ -1,1 +1,32 @@
 ﻿#include "ParticleManager.h"
+
+ParticleManager * ParticleManager::instance = NULL;
+
+ParticleManager* ParticleManager::getInstance()
+{
+	if (instance == NULL)
+		instance = new ParticleManager();
+	return instance;
+}
+
+void ParticleManager::AddPlistData(std::string fileName, std::string keyName)
+{
+	if (fileName == "" || keyName == "")
+		return;
+	
+	if (_plistMap.find(keyName) != _plistMap.end())
+		log("the keyName is exist already.");
+
+	auto plistData = FileUtils::getInstance()->getValueMapFromFile(fileName);
+	_plistMap[keyName] = plistData;
+}
+
+ValueMap ParticleManager::getPlistData(std::string keyname)
+{
+	if (keyname == "")
+		return;
+	if (_plistMap.find(keyname) == _plistMap.end())
+		log("can't find plistData by the specified keyName.");
+	else
+		return _plistMap[keyname];
+}
