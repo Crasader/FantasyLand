adff2be9c3454c09eba4b10eaa698faf5c862a3b
天 谢 <jsjtxietian@outlook.com>
2016-06-08 14:04:19 +0800

    MDC

--- Classes/MessageDispatchCenter.cpp
+++ Classes/MessageDispatchCenter.cpp
@@ -1,5 +1,6 @@
 ﻿#include "MessageDispatchCenter.h"
 
+
 std::set<std::string> MessageType;
 
 bool MessageDispatchCenter::init()
@@ -13,7 +14,49 @@ bool MessageDispatchCenter::init()
 	return true;
 }
 
-void MessageDispatchCenter::registerMessage(std::string messageType)
+void MessageDispatchCenter::registerMessage(std::string messageType /*callback*/)
 {
+	if(MessageType.find(messageType) == MessageType.end() )
+	{
+		std::cout << "param is invalid";
+		return;
+	}
+	
 
+		
+	/*if self.MessageQue[messageType] == nil then
+		self.MessageQue[messageType] = {}
+	end
+	local index = table.getn(self.MessageQue[messageType])
+	self.MessageQue[messageType][index + 1] = callback*/
+}
+
+void MessageDispatchCenter::removeMessage(std::string messageType/*callback*/)
+{
+	//if self.MessageType[messageType] == nil or type(callback) ~= "function" then
+	//	print("param is invalid")
+	//	return
+	//end
+	//for i, v in pairs(self.MessageQue[messageType]) do
+	//	if callback == v then
+	//		table.remove(self.MessageQue[messageType], i)
+	//		return
+	//	end
+	//end
+}
+
+void MessageDispatchCenter::dispatchMessage(std::string messageType/*callback*/)
+{
+	//if self.MessageType[messageType] == nil then
+	//	print("param is invalid")
+	//	return
+	//end
+
+	//	
+	//if self.MessageQue[messageType] == nil then
+	//	return
+	//end
+	//for i, v in pairs(self.MessageQue[messageType]) do
+	//	v(param)
+	//end
 }
