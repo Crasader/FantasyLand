816a366bebee94c0a81a3f7a3ebe05019d6381a9
天 谢 <jsjtxietian@outlook.com>
2016-06-10 11:07:45 +0800

    particlemanager

--- Classes/Helper.h
+++ Classes/Helper.h
@@ -24,9 +24,6 @@ struct
 	Actor * target;
 }msgStruct;
 
-
-
-
 float radNormalize(float rad);
 Vec2 getPosTable(Node *obj);
 Vec2 getNextStepPos(Vec2 curPos, Vec2 targetPos , float speed, float dt);
@@ -34,12 +31,13 @@ timerange createAnimationStruct(float var1, float var2, float var3);
 Animate3D * createAnimation(std::string file, float begin, float finish, float speed);
 msgStruct createKnockedMsgStruct(Actor * object);
 
-//function CreateEnumTable(tbl, index)
-//function removeAll(table)
-//void copytable()
+
 void delayExecute(Actor * target, void (*func)() ,float delay);
 
 float DEGREES_TO_RADIANS(float _ANGLE);
 float RADIANS_TO_DEGREES(float _ANGLE);
 
 //void print(std::map<> tab);
+//function CreateEnumTable(tbl, index)
+//function removeAll(table)
+//void copytable()
--- Classes/ParticleManager.h
+++ Classes/ParticleManager.h
@@ -15,7 +15,6 @@ public:
 
 private:
 	std::map<std::string,ValueMap> _plistMap;
-
 	static ParticleManager* instance;
 };
 
