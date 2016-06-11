bb06e4b63b186c1cf247a62417af7cc84b46d5a7
天 谢 <jsjtxietian@outlook.com>
2016-06-11 17:58:40 +0800

    MessageDis && gv

--- Classes/MessageDispatchCenter.h
+++ Classes/MessageDispatchCenter.h
@@ -24,6 +24,7 @@ public:
 
 	void registerMessage(std::string messageType /*callback*/);
 	void removeMessage(std::string messageType/*callback*/);
+
 	void dispatchMessage(std::string messageType /*param*/);
 
 private:
