b4fdf3aea7cf66c91bb98ab02a190903a5b877f2
QYH <qiuyuhangs@outlook.com>
2016-06-19 00:09:02 +0800

    0

--- Classes/MainMenuScene.h
+++ Classes/MainMenuScene.h
@@ -4,40 +4,23 @@
 
 USING_NS_CC;
 
-class MainMenuScene:public Layer
+class MainMenuScene :public Layer
 {
 public:
 	static cocos2d::Scene* createScene();
 	CREATE_FUNC(MainMenuScene);
 	virtual bool init();
 
-	void createLayer();
-	void onExit(Event *event);
-	void addLogo(Layer *layer);
-	void getLightSprite();
-	void addPointLight(Layer *layer);
-	void addButton(Layer *layer);
-	void addCloud(Layer *layer);
-	void addBg(Layer *layer);
+private:
+	ui::Button *_star;
+	ui::Button *_start;
+	ui::Button *_setting;
 
+	bool isTouchedStart;
+	Layer *_aboutLayer;
 
 	void addBackground();
 	void addButton();
 	void addAboutLayer();
 	void removeAboutLayer();
-private:
-	bool _isBloodLabelShowing;
-
-	Size _logoSize;
-	Sprite *_logo;
-	Sprite *_lightSprite;
-	PointLight *_pointLight;
-	Vec2 _prePosition;
-
-	ui::Button *_star;
-	ui::Button *_start;
-	ui::Button *_setting; 
-
-	bool isTouchedStart;
-	Layer *_aboutLayer;
 };
